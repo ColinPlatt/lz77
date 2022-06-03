7a39275d7200137fc4fa9a3687d0c8afaafee07d
Colin Platt <colingplatt@gmail.com>
2022-06-03 14:41:22 +0200

    adding tests

--- test/huffman.t.sol
+++ test/huffman.t.sol
@@ -25,14 +25,25 @@ contract huffmanTest is DSTest {
 
         Node memory rootNode = Node(abi.encodePacked(lNode.symbol, rNode.symbol), 0, lNode.weight + rNode.weight, abi.encode(lNode).toSliceBytes(), abi.encode(rNode).toSliceBytes());
 
-        emit log_bytes(rootNode.symbol);
-        emit log_uint(rootNode.weight);
+        emit log_string("check that we can pull the root node info");
+        assertEq0(rootNode.symbol, hex'6162');
+        assertEq(rootNode.weight, 11);
         emit log_uint(rootNode.left._ptr);
         emit log_uint(rootNode.left._len);
         emit log_uint(rootNode.right._ptr);
         emit log_uint(rootNode.right._len);
 
-        
+        emit log_string("check that we can pull the left node info");
+        assertEq0(abi.decode(rootNode.left.toByteString(), (Node)).symbol, hex'61');
+        assertEq(abi.decode(rootNode.left.toByteString(), (Node)).weight, 5);
+
+        emit log_string("check that we can pull the right node info");
+        assertEq0(abi.decode(rootNode.right.toByteString(), (Node)).symbol, hex'62');
+        assertEq(abi.decode(rootNode.right.toByteString(), (Node)).weight, 6);
+
+
+
+
 
     }
 
